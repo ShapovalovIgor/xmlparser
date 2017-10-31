6e680068d88f9f4c2b7517a1b566c555525b1ad5
Shapovalov <shapovalov0010@gmail.com>
2017-10-31 10:54:09 +0400

    Code refactoring.

--- src/main/java/com/haulmont/testtask/Constant.java
+++ src/main/java/com/haulmont/testtask/Constant.java
@@ -3,8 +3,4 @@ package com.haulmont.testtask;
 public class Constant {
     public static final String CREATE_TABLE_SCRIPT = "query/CREATE_TABLE.sql";
     public static final String DATA_SCRIPT = "query/DATA.sql";
-    public static final String DATE_MAX_ERROR = "Date can not be more than: ";
-    public static final String DATE_MIN_ERROR = "Date can not be earlier than: ";
-
-
 }
